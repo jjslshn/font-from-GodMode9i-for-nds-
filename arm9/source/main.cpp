b5d274bb622f057a49e843a8d39940459cd580ef
jjslshn <49503012+jjslshn@users.noreply.github.com>
2022-09-01 21:34:30 +0800

    Update main.cpp

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -19,12 +19,25 @@ int main() {
 	bgInitSub(2, BgType_Bmp8, BgSize_B8_256x256, 1, 0);
 
 	
-	Font();
-	iprintf(" nihao a 啊啊啊\n");//打印无效
 	
-	printf(3, 1,Palette::red,"你好，这是下屏显示的中文，后面是自动转行和符号n的转行\n这是转行符号之后的文字");
-	printop(1, -22,Palette::green,"大家好，这是上屏显示的中文，后面是自动转行和符号n的转行\n这是转行符号之后的文字");
+	Font();
+	
+	printop(Palette::white,"零");
+	printop(Palette::green,"第一行大苏打撒旦阿松大十大十大收到阿松大阿松大啊实打实打赏大苏打阿松大啊撒打算阿萨大的撒啊大的撒字");
+	printop(Palette::green,"第二行大苏打撒旦阿松大十大十大收到阿松大阿松大啊实打实打赏大苏打阿松大啊撒打算阿萨大的撒啊大的撒字");
+	printop(Palette::green,"第三行大苏打撒旦阿松大十大十大收到阿松大阿松大啊实打实打赏大苏打阿松大啊撒打算阿萨大的撒啊大的撒字");
+	printop(Palette::green,"第四行大苏打撒旦阿松大十大十大收到阿松大阿松大啊实打实打赏大苏打阿松大啊撒打算阿萨大的撒啊大的撒字");
 
+	
+	
+	u16 a =32;
+	printf(Palette::white,"零");
+	printf(Palette::red,"数字1只能答应一四川，覅二尺救兵1 1231\n23 \t带t%d",a);
+	printf(Palette::red,"数字2只能答应一四川，覅二尺救兵1 1231\n23 \t带t%d",a);
+	printf(Palette::red,"数字3只能答应一四川，覅二尺救兵1 1231\n23 \t带t%d",a);
+	printf(Palette::red,"数字4只能答应一四川，覅二尺救兵1 1231\n23 \t带t%d",a);
+	
+	
 
 			while (1) {
 				swiWaitForVBlank();
